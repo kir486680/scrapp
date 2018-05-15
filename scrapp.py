97b0afee12af5a77c7046a29d6b7d828bf842069
Kyrylo Kalashnikov <kir486680@gmail.com>
2018-05-15 09:34:37 -0400

    Update scrapp.py

--- scrapp.py
+++ scrapp.py
@@ -1,3 +1,4 @@
+import smtplib
 import requests
 from bs4 import BeautifulSoup
 import csv
@@ -52,7 +53,15 @@ class flat:
                     writer = csv.writer(f)
                     if 'Голосеевская' in a:
                         writer.writerow([arr[i] , a,])
+            self.send_mail(a)
+    def send_mail(a):
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login("kir486680@gmail.com", "My2ndmail")
 
+        msg = a
+        server.sendmail("kir486680@gmail.com", "THE EMAIL ADDRESS TO SEND TO", msg)
+        server.quit()
 def main():
     #new_flat = flat('https://dom.ria.com/ru/%D0%9A%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3/%D0%9F%D1%80%D0%BE%D0%B4%D0%B0%D0%B6%D0%B0/%D0%9A%D0%B2%D0%B0%D1%80%D1%82%D0%B8%D1%80%D1%8B/%D0%9A%D0%B2%D0%B0%D1%80%D1%82%D0%B8%D1%80%D0%B0/%D0%9E%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C/%D0%9A%D0%B8%D0%B5%D0%B2%D1%81%D0%BA%D0%B0%D1%8F/%D0%93%D0%BE%D1%80%D0%BE%D0%B4/%D0%9A%D0%B8%D0%B5%D0%B2/%D0%9C%D0%B5%D1%82%D1%80%D0%BE/%D0%94%D0%B5%D0%BC%D0%B8%D0%B5%D0%B2%D1%81%D0%BA%D0%B0%D1%8F/?page=5', 'wrap_desc' , 'blue' , 'https://dom.ria.com' )
     #new_flat.find()
